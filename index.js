f221ffd924d72377f493dfad6354e1e3e68de676
paulleerentveld <paul@veldnet.com.au>
2022-03-18 09:29:26 +1000

    add JS for lab

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 main.remove('#main');
+
 const newHeader = document.createElement('h1');
+
 newHeader.setAttribute('id', 'victory');
+
 newHeader.innerHTML = 'YOUR-NAME is the champion';
+
 document.body.append(newHeader);
